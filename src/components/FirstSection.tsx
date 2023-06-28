323e257264049bfa199e32871c5379a876785783
Jp-Bruno <jp.bruno.machado@gmail.com>
2023-06-28 12:09:57 -0300

    first section done

--- src/components/FirstSection.tsx
+++ src/components/FirstSection.tsx
@@ -1,57 +1,106 @@
 /* eslint-disable @next/next/no-img-element */
 import styled from "styled-components";
+import FirstSectionHexagons from "./FirstSectionsHexagons";
 
 const StyledSection = styled.section`
-  background-image: linear-gradient(#cbe1e3cc 10%, transparent);
-  position: relative;
+    border: solid 1px green;
+    display: grid;
+    grid-template-columns: 40% auto;
+    padding-top: 100px;
 
-  display: flex;
-  align-items: center;
-
-  .images-wrapper {
+  .first-section-list {
+    list-style-image: 20% url("/list_bullet.png");
     display: flex;
-    justify-content: center;
-    position: relative;
-    width: 100%;
+    flex-direction: column;
+    row-gap: 60px;
+    color: #15323A;
+    
 
-    img[alt="hexagono"] {
-      position: absolute;
-      width: 25%;
+    li {
+      display: flex;
+      flex-direction: row;
+      margin-left: -80px;
+      font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; 
 
-      &:first-of-type {
-        left: -10%;
-        bottom: 50px;
+      img {
+        width: 32%;
+        object-fit: contain;
       }
 
-      &:last-of-type {
-        top: 50px;
-        left: 80%;
+      h4 {
+        font-weight: 600;
+        font-size: 1rem;
       }
-    }
 
-    img[alt="banner"] {
-      position: relative;
-      width: 50%;
+      p {
+        font-size: 0.8rem;
+      }
     }
   }
+
+  .image-div {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    top: -50px;
+
+    & img[alt="banner"] {
+      position: absolute;
+      top: 17%;
+      left: 12%;
+    }
+  }
+
 `;
+
+const data = [
+  {
+    titulo: "PRIMEIROS PASSOS",
+    legenda: "Saiba tudo sobre a empresa.",
+  },
+  {
+    titulo: "CONHEÇA A EQUIPE",
+    legenda: "Conheça seus colegas de equipe.",
+  },
+  {
+    titulo: "MANUAIS E NORMAS",
+    legenda: "Normas e padrões para o desenvolvimento dos projetos.",
+  },
+  {
+    titulo: "CANAIS DE COMUNICAÇÃO",
+    legenda: "Como se comunicar na empresa.",
+  },
+  {
+    titulo: "ORION EDUCA",
+    legenda: "Treinamentos da Orion Arquitetura.",
+  },
+];
 
 export default function FirstSection() {
   return (
     <StyledSection>
-      <div className="images-wrapper">
-        <img
-          src="/hexagonos1.png"
-          alt="hexagono"
-        />
-        <img
-          src="/first-banner.png"
-          alt="banner"
-        />
-        <img
-          src="/hexagonos1.png"
-          alt="hexagono"
-        />
+      <div className="list-div">
+        <ul className="first-section-list">
+          {data.map((element: { titulo: string; legenda: string }) => {
+            return (
+              <li key={element.titulo}>
+                <img
+                  src={"/list_bullet.png"}
+                  alt="bullet"
+                  width={"20%"}
+                />
+                <div>
+                  <h4>{element.titulo}</h4>
+                  <p>{element.legenda}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="image-div">
+        <FirstSectionHexagons />
+        <img src="/first-banner.png" alt="banner" />
       </div>
     </StyledSection>
   );
---
+++ src/components/FirstSectionsHexagons.tsx
@@ -0,0 +1,98 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  position: relative;
+  right: 7%;
+
+  .list {
+    display: flex;
+    flex-direction: row-reverse;
+    position: relative;
+    margin-top: 2.5px;
+  }
+`;
+
+const Hexagon = styled.li<{ $timing?: number }>`
+  @keyframes colorr {
+    0% {
+      background-color: rgba(77, 133, 139, 0.2);
+    }
+
+    100% {
+      background-color: rgba(77, 133, 139, 0.5);
+    }
+  }
+
+  width: 90px;
+  height: 80px;
+  clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
+  position: relative;
+  margin-left: -19px;
+  animation: colorr;
+  animation-duration: 4s;
+  animation-delay: ${(props) => props.$timing}s;
+  animation-fill-mode: both;
+  animation-direction: alternate-reverse;
+  animation-iteration-count: infinite;
+
+  &:nth-of-type(odd) {
+    top: 41px;
+  }
+`;
+
+export default function FirstSectionHexagons() {
+  return (
+    <Wrapper>
+      <ul className="list">
+        {new Array(13).fill(2).map(() => {
+          return (
+            <Hexagon
+              key={Math.random() * 100}
+              $timing={Math.random() * 20}
+            />
+          );
+        })}
+      </ul>
+      <ul className="list">
+      {new Array(12).fill(2).map(() => {
+          return (
+            <Hexagon
+              key={Math.random() * 100}
+              $timing={Math.random() * 20}
+            />
+          );
+        })}
+      </ul>
+      <ul className="list">
+      {new Array(12).fill(2).map(() => {
+          return (
+            <Hexagon
+              key={Math.random() * 100}
+              $timing={Math.random() * 20}
+            />
+          );
+        })}
+      </ul>
+      <ul className="list">
+      {new Array(12).fill(2).map(() => {
+          return (
+            <Hexagon
+              key={Math.random() * 100}
+              $timing={Math.random() * 20}
+            />
+          );
+        })}
+      </ul>
+      <ul className="list">
+      {new Array(11).fill(2).map(() => {
+          return (
+            <Hexagon
+              key={Math.random() * 100}
+              $timing={Math.random() * 20}
+            />
+          );
+        })}
+      </ul>
+    </Wrapper>
+  );
+}
