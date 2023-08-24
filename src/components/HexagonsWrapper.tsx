5635e75ee5dc184e53b292701b2b2f5494596954
Jp-Bruno <jp.bruno.machado@gmail.com>
2023-08-24 16:51:26 -0300

    use client render to render hexagons based on window width

--- src/components/HexagonsWrapper.tsx
+++ src/components/HexagonsWrapper.tsx
@@ -1,5 +1,7 @@
+'use client'
 import Hexagon from "./Hexagon";
 import styles from "../styles/hexagons.module.scss";
+import { useEffect, useState } from "react";
 
 const funcionarios = [
     {
@@ -70,190 +72,365 @@ const funcionarios = [
 ];
 
 export default function HexagonsWrapper() {
-    return (
-        <div className={styles.hexagonsWrapper}>
-            <ul>
-                {new Array(2).fill(1).map((el) => (
+    const [windowWidth, setWindowWidth] = useState<null | number>(null)
+
+    useEffect(() => {
+        const resize = () => {
+            setWindowWidth(window.innerWidth)
+        }
+
+        resize()
+
+        window.addEventListener("resize", resize)
+
+        return () => {
+            window.removeEventListener("resize", resize)
+        }
+    }, [])
+
+    const breakpoint = 800
+
+    if (windowWidth < 800) {
+        return <div className={styles.hexagonsWrapper}>
+                     <ul>
+                    {new Array(2).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
+
                     <Hexagon
                         key={Math.random() * 1000}
+                        img="/imagens_funcionarios/katia.png"
+                        nome="Kátia Fugazza"
+                        cargo="Arquiteta Titular"
                     />
-                ))}
 
-                <Hexagon
-                    key={Math.random() * 1000}
-                    img="/imagens_funcionarios/katia.png"
-                    nome="Kátia Fugazza"
-                    cargo="Arquiteta Titular"
-                />
-
-                <Hexagon
-                    key={Math.random() * 1000}
-                    img="/imagens_funcionarios/carvalho.jpg"
-                    nome="Gabriela Carvalho"
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/carvalho.jpg"
+                        nome="Gabriela Carvalho"
                         cargo="Projetista"
-                />
+                    />
 
-                <Hexagon
-                    key={Math.random() * 1000}
-                    img="/imagens_funcionarios/camila.webp"
-                    nome="Camila Oliveira"
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/camila.webp"
+                        nome="Camila Oliveira"
                         cargo="Designer de serviços"
-                />
-            </ul>
-            <ul>
-                {new Array(1).fill(1).map((el) => (
+                    />
+                </ul>
+                <ul>
+                    {new Array(1).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
+
                     <Hexagon
                         key={Math.random() * 1000}
+                        img="/imagens_funcionarios/paulo.jpg"
+                        nome="Paulo Fugazza"
+                        cargo="Informática e Administrador"
                     />
-                ))}
 
-                <Hexagon
-                    key={Math.random() * 1000}
-                    img="/imagens_funcionarios/paulo.jpg"
-                    nome="Paulo Fugazza"
-                    cargo="Informática e Administrador"
-                />
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/marcosmarques.webp"
+                        nome="Marcos Marques"
+                        cargo="Gerente de Projetos e Engenheiro"
+                    />
 
-                <Hexagon
-                    key={Math.random() * 1000}
-                    img="/imagens_funcionarios/marcosmarques.webp"
-                    nome="Marcos Marques"
-                    cargo="Gerente de Projetos e Engenheiro"
-                />
-
-                <Hexagon
-                    key={Math.random() * 1000}
-                    img="/imagens_funcionarios/marcos.webp"
-                    nome="Marcos Lima"
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/marcos.webp"
+                        nome="Marcos Lima"
                         cargo="Projetista"
-                />
-            </ul>
-            <ul>
-                {new Array(2).fill(1).map((el) => (
+                    />
+                </ul>
+                <ul>
+                    {new Array(2).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
+
                     <Hexagon
                         key={Math.random() * 1000}
+                        img="/imagens_funcionarios/walter.webp"
+                        nome="Walter Jr"
+                        cargo="Eng. Elétrico e de Segurança"
                     />
-                ))}
 
-                <Hexagon
-                    key={Math.random() * 1000}
-                    img="/imagens_funcionarios/walter.webp"
-                    nome="Walter Jr"
-                    cargo="Eng. Elétrico e de Segurança"
-                />
-
-                <Hexagon
-                    key={Math.random() * 1000}
-                    img="/imagens_funcionarios/joaopedro.webp"
-                    nome="João Pedro"
-                    cargo="Acadêmico de Informática"
-                />
-            </ul>
-            <ul>
-                {new Array(1).fill(1).map((el) => (
                     <Hexagon
                         key={Math.random() * 1000}
+                        img="/imagens_funcionarios/joaopedro.webp"
+                        nome="João Pedro"
+                        cargo="Acadêmico de Informática"
                     />
-                ))}
+                </ul>
+                <ul>
+                    {new Array(1).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
 
-                <Hexagon
-                    key={Math.random() * 1000}
-                    img="/imagens_funcionarios/diego.jpg"
-                    nome="Diego"
-                    cargo="Orçamentista"
-                />
-
-                <Hexagon
-                    key={Math.random() * 1000}
-                    img="/imagens_funcionarios/carol.webp"
-                    nome="Caroline Freire"
-                    cargo="Projetista"
-                />
-
-                <Hexagon
-                    key={Math.random() * 1000}
-                    img="/imagens_funcionarios/alexia.png"
-                    nome="Alexia"
-                    cargo="Acadêmica de Arquitetura"
-                />
-            </ul>
-            <ul>
-                {new Array(2).fill(1).map((el) => (
                     <Hexagon
                         key={Math.random() * 1000}
+                        img="/imagens_funcionarios/diego.jpg"
+                        nome="Diego"
+                        cargo="Orçamentista"
                     />
-                ))}
 
-                <Hexagon
-                    key={Math.random() * 1000}
-                    img="/imagens_funcionarios/andrea.webp"
-                    nome="Andrea Suzuki"
-                    cargo="Administrativo"
-                />
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/carol.webp"
+                        nome="Caroline Freire"
+                        cargo="Projetista"
+                    />
 
-                <Hexagon
-                    key={Math.random() * 1000}
-                    img="/imagens_funcionarios/julia.jpg"
-                    nome="Julia Telles"
-                    cargo="Projetista"
-                />
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/alexia.png"
+                        nome="Alexia"
+                        cargo="Acadêmica de Arquitetura"
+                    />
+                </ul>
+                <ul>
+                    {new Array(2).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
 
-                <Hexagon
-                    key={Math.random() * 1000}
-                    img="/imagens_funcionarios/naomi.png"
-                    nome="Naomi"
-                    cargo="Acadêmica de Arquitetura"
-                />
-            </ul>
-            <ul>
-                {new Array(2).fill(1).map((el) => (
                     <Hexagon
                         key={Math.random() * 1000}
+                        img="/imagens_funcionarios/andrea.webp"
+                        nome="Andrea Suzuki"
+                        cargo="Administrativo"
                     />
-                ))}
 
-                <Hexagon
-                    key={Math.random() * 1000}
-                    img="/imagens_funcionarios/michel.webp"
-                    nome="Michel Carvalho"
-                    cargo="Jovem Aprendiz"
-                />
-            </ul>
-            <ul>
-                {new Array(2).fill(1).map((el) => (
                     <Hexagon
                         key={Math.random() * 1000}
+                        img="/imagens_funcionarios/julia.jpg"
+                        nome="Julia Telles"
+                        cargo="Projetista"
                     />
-                ))}
-            </ul>
-            <ul>
-                {new Array(1).fill(1).map((el) => (
+
                     <Hexagon
                         key={Math.random() * 1000}
+                        img="/imagens_funcionarios/naomi.png"
+                        nome="Naomi"
+                        cargo="Acadêmica de Arquitetura"
                     />
-                ))}
-            </ul>
-            <ul>
-                {new Array(2).fill(1).map((el) => (
+                </ul>
+                <ul>
+                    {new Array(2).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
+
                     <Hexagon
                         key={Math.random() * 1000}
+                        img="/imagens_funcionarios/michel.webp"
+                        nome="Michel Carvalho"
+                        cargo="Jovem Aprendiz"
                     />
-                ))}
-            </ul>
-            <ul>
-                {new Array(1).fill(1).map((el) => (
-                    <Hexagon
-                        key={Math.random() * 1000}
-                    />
-                ))}
-            </ul>
-            <ul>
-                {new Array(2).fill(1).map((el) => (
-                    <Hexagon
-                        key={Math.random() * 1000}
-                    />
-                ))}
-            </ul>
+                </ul>
         </div>
-    );
+    }
+
+    if (windowWidth > 800) {
+        return (
+            <div className={styles.hexagonsWrapper}>
+                <ul>
+                    {new Array(2).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
+
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/katia.png"
+                        nome="Kátia Fugazza"
+                        cargo="Arquiteta Titular"
+                    />
+
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/carvalho.jpg"
+                        nome="Gabriela Carvalho"
+                        cargo="Projetista"
+                    />
+
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/camila.webp"
+                        nome="Camila Oliveira"
+                        cargo="Designer de serviços"
+                    />
+                </ul>
+                <ul>
+                    {new Array(1).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
+
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/paulo.jpg"
+                        nome="Paulo Fugazza"
+                        cargo="Informática e Administrador"
+                    />
+
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/marcosmarques.webp"
+                        nome="Marcos Marques"
+                        cargo="Gerente de Projetos e Engenheiro"
+                    />
+
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/marcos.webp"
+                        nome="Marcos Lima"
+                        cargo="Projetista"
+                    />
+                </ul>
+                <ul>
+                    {new Array(2).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
+
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/walter.webp"
+                        nome="Walter Jr"
+                        cargo="Eng. Elétrico e de Segurança"
+                    />
+
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/joaopedro.webp"
+                        nome="João Pedro"
+                        cargo="Acadêmico de Informática"
+                    />
+                </ul>
+                <ul>
+                    {new Array(1).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
+
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/diego.jpg"
+                        nome="Diego"
+                        cargo="Orçamentista"
+                    />
+
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/carol.webp"
+                        nome="Caroline Freire"
+                        cargo="Projetista"
+                    />
+
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/alexia.png"
+                        nome="Alexia"
+                        cargo="Acadêmica de Arquitetura"
+                    />
+                </ul>
+                <ul>
+                    {new Array(2).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
+
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/andrea.webp"
+                        nome="Andrea Suzuki"
+                        cargo="Administrativo"
+                    />
+
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/julia.jpg"
+                        nome="Julia Telles"
+                        cargo="Projetista"
+                    />
+
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/naomi.png"
+                        nome="Naomi"
+                        cargo="Acadêmica de Arquitetura"
+                    />
+                </ul>
+                <ul>
+                    {new Array(2).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
+
+                    <Hexagon
+                        key={Math.random() * 1000}
+                        img="/imagens_funcionarios/michel.webp"
+                        nome="Michel Carvalho"
+                        cargo="Jovem Aprendiz"
+                    />
+                </ul>
+                <ul>
+                    {new Array(2).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
+                </ul>
+                <ul>
+                    {new Array(1).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
+                </ul>
+                <ul>
+                    {new Array(2).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
+                </ul>
+                <ul>
+                    {new Array(1).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
+                </ul>
+                <ul>
+                    {new Array(2).fill(1).map((el) => (
+                        <Hexagon
+                            key={Math.random() * 1000}
+                        />
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+
+
+
 }
