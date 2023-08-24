5393e7c888e56877279890fbdff02d2b1efaf1bc
Jp-Bruno <jp.bruno.machado@gmail.com>
2023-08-24 17:00:26 -0300

    mobile version done

--- src/components/sections/Section3.tsx
+++ src/components/sections/Section3.tsx
@@ -6,16 +6,19 @@ const cardsData = [
         title: "Bim",
         description:
             "O uso do BIM (Building Information Modeling) na Orion Arquitetura revolucionou a maneira como projetamos e construímos. Através dessa metodologia inovadora, somos capazes de criar modelos virtuais tridimensionais altamente precisos e detalhados, que integram todas as informações relevantes de um projeto arquitetônico, desde elementos estruturais até sistemas elétricos e hidráulicos.",
+        buttonText: "Clique aqui para acessar o conteúdo Bim.",
     },
     {
         title: "Manuais e Normas",
         description:
             "Nossa biblioteca auxilia na orientação da implementação do Building Information Modeling em projetos arquitetônicos e garante um padronização das criações e gestão de modelos. Há benefícios significativos ao utilizá-la, como a melhoria da colaboração entre equipes, a redução de erros e retrabalhos, e o aumento da eficiência geral do processo de projeto e construção.",
+            buttonText: "Clique aqui para acessar nossa biblioteca"
     },
     {
         title: "Canais de Comunicação",
         description:
             "Todos da Orion Arquitetura possuem um acesso para utilizar a plataforma Teams da Microsoft. Dentro da plataforma há a subdivisão de projetos e pelo Planner (Microsoft) determinamos as tarefas que precisam ser cumpridas dentro dessas subdivisões. Para o compartilhamento de arquivos dentro da equipe, utilizamos o Onedrive ou o Sharepoint. E para as revisões internas, utilizamos o Construcode. Caso não saiba como utilizar os programas citados acima, veja os vídeos na aba Orion Educa.",
+            buttonText:"clique aqui para saber mais sobre nosso HUB"
     },
 ];
 
@@ -29,9 +32,7 @@ export default function Section3() {
                             key={card.title}
                             title={card.title}
                             buttonText={
-                                card.title === "Manuais e Normas"
-                                    ? "Clique aqui para acessar nossa biblioteca"
-                                    : ""
+                                card.buttonText
                             }
                         >
                             {card.description}
@@ -46,8 +47,8 @@ export default function Section3() {
                     buttonHref="https://educa.orionarquitetura.com/"
                 >
                     <p>
-                        Nesta página você encontrará todos os cursos de educação continuada
-                        produzidos pela Orion!
+                        Nesta página você encontrará todos os cursos de educação continuada produzidos pela
+                        Orion!
                     </p>
                     <p style={{ textAlign: "center", margin: "16px 0" }}>
                         <i>
@@ -55,8 +56,8 @@ export default function Section3() {
                         </i>
                     </p>
                     <p>
-                        Seu acesso vai de dicas até cursos específicos das disciplinas que
-                        usamos no nosso dia a dia.
+                        Seu acesso vai de dicas até cursos específicos das disciplinas que usamos no nosso dia a
+                        dia.
                     </p>
                 </Section3ListItem>
             </ul>
